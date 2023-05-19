0f35dc87613c7349d987b77c72cd33ec29a96b5a
Brad Johnson <bradleywjohnson@outlook.com>
2023-05-18 23:05:45 -0500

    refactored code to allow use with stdin as well as files

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,21 @@
 use clap::{App, Arg};
 use regex::Regex;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-fn main() {
-    let args = App::new("grep-lite")
-        .version("0.1")
-        .about("searches for patterns")
-        .arg(
-            Arg::with_name("pattern")
-                .help("the pattern to search for")
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("input")
-                .help("the file to search")
-                .takes_value(true)
-                .required(true),
-        )
-        .get_matches();
-
-    let pattern = args.value_of("pattern").unwrap();
-    let re = Regex::new(pattern).unwrap();
-    let ctx_lines = 2; // TODO: take in an arg for this
-
-    let input = args.value_of("input").unwrap();
-    let f = File::open(input).unwrap();
-    let mut reader = BufReader::new(f);
-
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex, ctx_lines: usize) {
     let mut tags: Vec<usize> = vec![];
     let mut ctx: Vec<Vec<(usize, String)>> = vec![];
 
-    for (i, line_) in reader.by_ref().lines().enumerate() {
-        let line = line_.unwrap();
+    let lines = reader
+        .lines()
+        .take_while(|line| line.as_ref().ok().is_some())
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
+
+    for (i, line) in lines.iter().enumerate() {
         match re.find(&line) {
             Some(_) => {
                 tags.push(i);
@@ -50,26 +31,56 @@ fn main() {
         return;
     }
 
-    reader.seek(std::io::SeekFrom::Start(0)).unwrap();
-
-    let mut line: String;
-    for (i, line_) in reader.lines().enumerate() {
-        line = line_.unwrap();
+    for (i, line) in lines.iter().enumerate() {
         for (j, tag) in tags.iter().enumerate() {
             let lower_bound = tag.saturating_sub(ctx_lines);
             let upper_bound = tag + ctx_lines;
 
             if (i >= lower_bound) && (i <= upper_bound) {
-                let line_as_string = String::from(&line);
+                let line_as_string = String::from(line);
                 let local_ctx = (i, line_as_string);
                 ctx[j].push(local_ctx);
             }
         }
     }
-
     for local_ctx in ctx.iter() {
         for &(i, ref line) in local_ctx.iter() {
             println!("{}: {}", i + 1, line)
         }
+    }
+}
+
+fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("the pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("the file to search")
+                .takes_value(true)
+                .required(false),
+        )
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+    let ctx_lines = 2; // TODO: take in an arg for this
+
+    let input = args.value_of("input").unwrap_or("-");
+
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re, ctx_lines);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re, ctx_lines);
     }
 }
